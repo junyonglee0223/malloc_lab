a5b08b3c37a67af382c8dcca4fb896adfafcb308
junyonglee <ljyong3339@gmail.com>
2024-03-27 23:13:14 +0900

    change my name

--- mm.c
+++ mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "junyongTeam",
     /* First member's full name */
-    "Harry Bovik",
+    "JunyongLee",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "ljyong3339@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
